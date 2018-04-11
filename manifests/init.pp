053c31a04f7c5052a416c569081bada5478d0379
Petri Lammi <petri.lammi@tietoteema.fi>
2018-04-11 20:29:17 +0300

    A little bit of tuning
    Signed-off-by: Petri Lammi <petri.lammi@tietoteema.fi>

--- manifests/init.pp
+++ manifests/init.pp
@@ -23,7 +23,14 @@
 # === Copyright
 #
 # Copyright 2018 Petri Lammi, Samuli Seppänen
-class windows_wmf
+#
+# Credits to red-gate, https://github.com/red-gate/puppet-wmf for working code and ideas
+class windows_wmf(
+
+  $wmf_version = $windows_wmf::params::version,
+  $temp_folder = $windows_wmf::params::temp_folder
+
+  ) inherits windows_wmf::params
 {
 
   $dotnetkb = $::facts['os']['release']['full'] ? {
@@ -41,20 +48,38 @@ class windows_wmf
     kb       => $dotnetkb,
   }
 
-  $wmf5kb = $::facts['os']['release']['full'] ? {
-    '7'       => 'KB3134760',
-    '8.1'     => 'KB3134758',
-    '2008 R2' => 'KB3134760',
-    '2012'    => 'KB3134759',
-    '2012 R2' => 'KB3134758',
-    default   => undef,
+  unless has_key($windows_wmf::params::urls, "${version}") {
+    fail "This version of WMF is not supported: ${version}"
   }
 
-  windows_updates::kb { 'WMF5.0':
-    ensure   => 'present',
-    kb       => $wmf5kb,
-    require  => Windows_updates::Kb['.NET 4.7.1'],
+  unless has_key($windows_wmf::params::urls["${version}"], $::operatingsystemrelease) {
+    fail "This OS (${::operatingsystemrelease}) is not supported for WMF ${version}"
   }
+  
+  $download_url = $windows_wmf::params::urls["${version}"][$::operatingsystemrelease]
+  $kb_number = $download_url.match(/(KB\d+)/)[1]
+
+  file { $temp_folder:
+    ensure => directory,
+  } -> 
+
+  file { "${temp_folder}/wmf_${version}.msu":
+    ensure  => 'present',
+    source  => $download_url,
+  } -> 
+  
+  exec { "Install WMF ${version}":
+    # Need to use "cmd.exe /c Start" to block the exec task until the wusa.exe exit.
+    command  => "cmd.exe /c Start /WAIT wusa.exe \"${temp_folder}/wmf_${version}.msu\" /quiet /norestart",
+    returns  => [0, 3010],
+    unless   => "cmd.exe /c C:\\Windows\\System32\\wbem\\WMIC.exe qfe | findstr ${kb_number}",
+    path     => 'C:/Windows/System32/',
+    provider => 'windows',
+    #require  => Windows_updates::kb['.NET 4.7.1'],
+    
+  } ~> 
+
+  reboot { "Reboot after installation of WMF version ${version}": }
+
 }
-
 

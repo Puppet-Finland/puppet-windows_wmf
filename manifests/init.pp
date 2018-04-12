779ad61a6d11377603631d26696a4c05fe1c2a2b
Petri Lammi <petri.lammi@tietoteema.fi>
2018-04-12 07:28:20 +0300

    A little bit of linting
    Signed-off-by: Petri Lammi <petri.lammi@tietoteema.fi>

--- manifests/init.pp
+++ manifests/init.pp
@@ -44,40 +44,40 @@ class windows_wmf(
   }
 
   windows_updates::kb { '.NET 4.7.1':
-    ensure   => 'present',
-    kb       => $dotnetkb,
+    ensure => 'present',
+    kb     => $dotnetkb,
   }
 
   unless has_key($windows_wmf::params::urls, "${version}") {
-    fail "This version of WMF is not supported: ${version}"
+    fail("This version of WMF is not supported: ${version}")
   }
 
   unless has_key($windows_wmf::params::urls["${version}"], $::operatingsystemrelease) {
-    fail "This OS (${::operatingsystemrelease}) is not supported for WMF ${version}"
+    fail("This OS (${::operatingsystemrelease}) is not supported for WMF ${version}")
   }
-  
+
   $download_url = $windows_wmf::params::urls["${version}"][$::operatingsystemrelease]
   $kb_number = $download_url.match(/(KB\d+)/)[1]
 
   file { $temp_folder:
     ensure => directory,
-  } -> 
+  }
 
-  file { "${temp_folder}/wmf_${version}.msu":
-    ensure  => 'present',
-    source  => $download_url,
-  } -> 
-  
-  exec { "Install WMF ${version}":
+  -> file { "${temp_folder}/wmf_${version}.msu":
+    ensure => 'present',
+    source => $download_url,
+  }
+
+  -> exec { "Install WMF ${version}":
     # Need to use "cmd.exe /c Start" to block the exec task until the wusa.exe exit.
     command  => "cmd.exe /c Start /WAIT wusa.exe \"${temp_folder}/wmf_${version}.msu\" /quiet /norestart",
     returns  => [0, 3010],
     unless   => "cmd.exe /c C:\\Windows\\System32\\wbem\\WMIC.exe qfe | findstr ${kb_number}",
     path     => 'C:/Windows/System32/',
     provider => 'windows',
-  } ~> 
+  }
 
-  reboot { "Reboot after installation of WMF version ${version}": }
+  ~> reboot { "Reboot after installation of WMF version ${version}": }
 
 }
 
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,15 +1,18 @@
+#
+# == Class: windows_wmf::params
+#
+# Parameters for windows_wmf module
 class windows_wmf::params {
   $version = '5.0'
   $temp_folder = 'c:\\windows\\temp'
-  
   $urls = {
     '5.0' => {
       '2012' => 'https://download.microsoft.com/download/2/C/6/2C6E1B4A-EBE5-48A6-B225-2D2058A9CEFB/W2K12-KB3134759-x64.msu',
       '2012 R2' => 'https://download.microsoft.com/download/2/C/6/2C6E1B4A-EBE5-48A6-B225-2D2058A9CEFB/Win8.1AndW2K12R2-KB3134758-x64.msu',
     },
-     '5.1' => {
+    '5.1' => {
       '2012' => 'https://download.microsoft.com/download/6/F/5/6F5FF66C-6775-42B0-86C4-47D41F2DA187/W2K12-KB3191565-x64.msu',
       '2012 R2' => 'https://download.microsoft.com/download/6/F/5/6F5FF66C-6775-42B0-86C4-47D41F2DA187/Win8.1AndW2K12R2-KB3191564-x64.msu',
-     }
+    }
   }
 }

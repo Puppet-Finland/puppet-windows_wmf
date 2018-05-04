745b066bbfc3662b2f89e652bccc260782300eca
Petri Lammi <petri.lammi@tietoteema.fi>
2018-05-04 18:40:55 +0300

    Tweak condition
    Signed-off-by: Petri Lammi <petri.lammi@tietoteema.fi>

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 #
 # == Class: windows_wmf
 #
-# Base profile for windowses
+# Install Windows Management Framework
 #
 # === Parameters
 #
@@ -28,56 +28,48 @@
 class windows_wmf(
 
   $wmf_version = $windows_wmf::params::version,
-  $temp_folder = $windows_wmf::params::temp_folder
-
+  $temp_folder = $windows_wmf::params::temp_folder,
+  $dotnetkb    = $windows_wmf::params::dotnetkb,
   ) inherits windows_wmf::params
 {
-
-  $dotnetkb = $::facts['os']['release']['full'] ? {
-    '7'       => 'KB4033342',
-    '8.1'     => 'KB4033369',
-    '2008 R2' => 'KB4033342',
-    '2012'    => 'KB4033345',
-    '2012 R2' => 'KB4033369',
-    '2016'    => 'KB4033369',
-    default   => undef,
-  }
 
   windows_updates::kb { '.NET 4.7.1':
     ensure => 'present',
     kb     => $dotnetkb,
   }
 
-  unless has_key($windows_wmf::params::urls, "${version}") {
-    fail("This version of WMF is not supported: ${version}")
+  unless (versioncmp($wmfmajorversion, '5') > 0 or versioncmp($wmfmajorversion, '5') == 1 ) { 
+
+    unless has_key($windows_wmf::params::urls, "${version}") {
+      fail("This version of WMF is not supported: ${version}")
+    }
+
+    unless has_key($windows_wmf::params::urls["${version}"], $::operatingsystemrelease) {
+      fail("This OS (${::operatingsystemrelease}) is not supported for WMF ${version}")
+    }
+
+    $download_url = $windows_wmf::params::urls["${version}"][$::operatingsystemrelease]
+    $kb_number = $download_url.match(/(KB\d+)/)[1]
+
+    file { $temp_folder:
+      ensure => directory,
+    }
+
+    -> file { "${temp_folder}/wmf_${version}.msu":
+      ensure => 'present',
+      source => $download_url,
+    }
+    
+    -> exec { "Install WMF ${version}":
+      # Need to use "cmd.exe /c Start" to block the exec task until the wusa.exe exit.
+      command  => "cmd.exe /c Start /WAIT wusa.exe \"${temp_folder}/wmf_${version}.msu\" /quiet /norestart",
+      returns  => [0, 3010],
+      unless   => "cmd.exe /c C:\\Windows\\System32\\wbem\\WMIC.exe qfe | findstr ${kb_number}",
+      path     => 'C:/Windows/System32/',
+      provider => 'windows',
+    }
+    
+    ~> reboot { "Reboot after installation of WMF version ${version}": }
   }
-
-  unless has_key($windows_wmf::params::urls["${version}"], $::operatingsystemrelease) {
-    fail("This OS (${::operatingsystemrelease}) is not supported for WMF ${version}")
-  }
-
-  $download_url = $windows_wmf::params::urls["${version}"][$::operatingsystemrelease]
-  $kb_number = $download_url.match(/(KB\d+)/)[1]
-
-  file { $temp_folder:
-    ensure => directory,
-  }
-
-  -> file { "${temp_folder}/wmf_${version}.msu":
-    ensure => 'present',
-    source => $download_url,
-  }
-
-  -> exec { "Install WMF ${version}":
-    # Need to use "cmd.exe /c Start" to block the exec task until the wusa.exe exit.
-    command  => "cmd.exe /c Start /WAIT wusa.exe \"${temp_folder}/wmf_${version}.msu\" /quiet /norestart",
-    returns  => [0, 3010],
-    unless   => "cmd.exe /c C:\\Windows\\System32\\wbem\\WMIC.exe qfe | findstr ${kb_number}",
-    path     => 'C:/Windows/System32/',
-    provider => 'windows',
-  }
-
-  ~> reboot { "Reboot after installation of WMF version ${version}": }
-
 }
 

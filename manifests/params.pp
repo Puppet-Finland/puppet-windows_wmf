4cab921c20839684ac62509a1f35cd3d82ab3af5
Petri Lammi <petri.lammi@tietoteema.fi>
2018-06-14 11:34:12 +0300

    Add more releases
    Signed-off-by: Petri Lammi <petri.lammi@tietoteema.fi>

--- manifests/params.pp
+++ manifests/params.pp
@@ -11,8 +11,21 @@ class windows_wmf::params {
       '2012 R2' => 'https://download.microsoft.com/download/2/C/6/2C6E1B4A-EBE5-48A6-B225-2D2058A9CEFB/Win8.1AndW2K12R2-KB3134758-x64.msu',
     },
     '5.1' => {
-      '2012' => 'https://download.microsoft.com/download/6/F/5/6F5FF66C-6775-42B0-86C4-47D41F2DA187/W2K12-KB3191565-x64.msu',
+      '7'       => 'https://download.microsoft.com/download/6/F/5/6F5FF66C-6775-42B0-86C4-47D41F2DA187/Win7AndW2K8R2-KB3191566-x64.zip',
+      '8.1'     => 'https://download.microsoft.com/download/6/F/5/6F5FF66C-6775-42B0-86C4-47D41F2DA187/Win8.1AndW2K12R2-KB3191564-x64.msu',
+      '2008 R2' => 'https://download.microsoft.com/download/6/F/5/6F5FF66C-6775-42B0-86C4-47D41F2DA187/Win7AndW2K8R2-KB3191566-x64.zip',
+      '2012'    => 'https://download.microsoft.com/download/6/F/5/6F5FF66C-6775-42B0-86C4-47D41F2DA187/W2K12-KB3191565-x64.msu',
       '2012 R2' => 'https://download.microsoft.com/download/6/F/5/6F5FF66C-6775-42B0-86C4-47D41F2DA187/Win8.1AndW2K12R2-KB3191564-x64.msu',
     }
+  }
+
+  $dotnetkb = $::facts['os']['release']['full'] ? {
+    '7'       => 'KB4033342',
+    '8.1'     => 'KB4033369',
+    '2008 R2' => 'KB4033342',
+    '2012'    => 'KB4033345',
+    '2012 R2' => 'KB4033369',
+    '2016'    => 'KB4033369',
+    default   => undef,
   }
 }
